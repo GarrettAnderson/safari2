90e746f3be983308444934844a8d2815ad6fb0cf
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-03-20 23:16:07 -0400

    working on getting the lion species specifically. not sure if i debugged it correctly bc i dont get an error in local host but i dont get the specific object for the lio only. played aroun with the synatx but not getting a 404 error. I created a new animal but was testing and forgot to add values so its all nil

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -7,7 +7,7 @@ class AnimalsController < ApplicationController
 
   # GET /animals/1
   def show
-    @animal = Animal.find(params[:id])
+    @animal = Animal.find(params[:species] == "Lion")
 
     render json: @animal
   end
@@ -22,4 +22,29 @@ class AnimalsController < ApplicationController
       render json: @animal.errors, status: :unprocessable_entity
     end
   end
+
+  # PATCH/PUT /animals/1
+  # def update
+  #   @animal = Animal.find(params[:id])
+
+  #   if @animal.update(animal_params)
+  #     render json: @animal
+  #   else
+  #     render json: @animal.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # # DELETE /animals/1
+  # def destroy
+  #   @animal = Animal.find(params[:id])
+
+  #   @animal.destroy
+  # end
+
+  # private
+
+  # # Only allow a trusted parameter "white list" through.
+  # def animal_params
+  #   params.require(:animal).permit(:species, :last_seen_location, :seen_count)
+  # end
 end
